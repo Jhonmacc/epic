1ebe549897d2461c8bccf05c57dbbf370eea0775
Jhon Amorim <jhon.amorim@simeon.com.br>
2023-07-24 07:27:11 -0300

    Envio app

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card mt-4">
+    <img class="card-img-top">
+    <div class="card-body">
+      <h4 class="card-title">{{ title }}</h4>
+      <p class="card-text">{{ version }}</p>
+    </div>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+    setup() {
+      return {
+        title: 'Olá, seja bem vindo!',
+        version: '1.0.0' // Aqui estou definindo uma versão fixa como exemplo.
+      };
+    }
+  }
+  </script>
+  
